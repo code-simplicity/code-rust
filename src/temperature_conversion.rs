5394f65503d7af10cb8013b679b2de74ffa8d23a
peiyi.du <peiyi.du@huilianyi.com>
2024-09-08 19:41:08 +0800

    :art: 温度转换

--- src/temperature_conversion.rs
+++ src/temperature_conversion.rs
@@ -1,7 +1,18 @@
+use std::io;
+
+/// 摄氏度和华氏度的转变
 pub fn main() {
-    // 摄氏度和华氏度的转变
-    let mut temp = 35.0;
-    println!("摄氏度: {}", temp);
-    temp = temp * 9.0 / 5.0 + 32.0;
-    println!("华氏度: {}", temp);
+    println!("请输入摄氏温度");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("数据无效");
+    let celsius: f64 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("输入无效，请重新输入");
+            return;
+        }
+    };
+    println!("摄氏度: {}", celsius);
+    let fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+    println!("华氏度: {}", fahrenheit);
 }
