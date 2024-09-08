c8246a71ae83e6fb1d3aa18a702cb6e2b7c252a5
peiyi.du <peiyi.du@huilianyi.com>
2024-09-08 14:54:16 +0800

    :art: 第一次提交代码

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+mod data_types;
+mod guess_number;
+mod variables_and_mutability;
+mod functions;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    //
+    // let mut  x = 5;
+    // println!("原始数据 {x}");
+    //
+    // x = 6;
+    //
+    // println!("可变数据 {x}");
+    //
+    // const MAX_POINTS: i32 = 100_000;
+
+    // 生成随机数
+    // guess_number::main();
+
+    // 常量学习
+    // variables_and_mutability::main();
+
+    // 数据类型学习
+    // data_types::main();
+
+    functions::main();
 }
