7f47fedf7846bc08b1d4ff8a2feb53b354809c53
peiyi.du <peiyi.du@huilianyi.com>
2024-09-08 14:54:07 +0800

    :art: 第一次提交代码

---
+++ src/variables_and_mutability.rs
@@ -0,0 +1,38 @@
+// 变量和可变性
+pub fn main() {
+    // let mut  x = 5;
+    //
+    // println!("x = {}", x);
+    //
+    // x = 6;
+    //
+    // println!("x = {}", x);
+    //
+    // let spaces = "   ";
+    // let spaces = spaces.len();
+    // println!("spaces = {}", spaces);
+    //
+    // let mut spaces = "   ";
+    // println!("spaces = {}", spaces);
+    //
+    // // 声明常量，用在该使用的地方不可修改
+    // // 语法规法，rust对常量的规范是大写并且是下划线
+    // const MAX_POINTS: i32 = 100_000;
+
+    // let x = 5;
+    // let x = x + 1;
+    //
+    // {
+    //     // 形成新的作用域，不会影响到原来的值
+    //     let x = x * 2;
+    //     println!("x inner scope = {}", x);
+    // }
+    // println!("x = {}", x);
+    //
+    // // 类型问题
+    // let spaces = "   ";
+    // println!("spaces {spaces}");
+    //
+    // spaces = spaces.len();
+    // println!("spaces length {spaces}");
+}
